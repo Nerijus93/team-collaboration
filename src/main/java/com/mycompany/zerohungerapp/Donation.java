e4e2a3545b8d8146661ddad88e1cf2dad12449f4
Nerijus93 <nerijus.kmitas@gmail.com>
2025-11-15 20:19:37 +0000

    Refactor Donation class and update author
    Updated author information and preserved class structure.

--- src/main/java/com/mycompany/zerohungerapp/Donation.java
+++ src/main/java/com/mycompany/zerohungerapp/Donation.java
@@ -6,7 +6,7 @@ package com.mycompany.main;
 
 /**
  *
- * @author ITwork
+ * @author Nerijus Kmitas
  */
 public class Donation {
 
@@ -52,4 +52,5 @@ public class Donation {
                 + "\nImage: " + image;
     }
 }
+
 
